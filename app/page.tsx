3ae9a73843e1829d5fe87b603611ebc0ecb89062
ElHombreZ <kaypro1932@gmail.com>
2026-01-15 17:55:39 -0800

    Created Resources File

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,14 @@ export default function Home() {
       >
         Go to Leadership Page →
       </Link>
+
+      {/* Link to the Resources page */}
+      <Link
+        href="/resources"
+        className="mt-4 inline-block text-blue-600 hover:underline"
+      >
+        Go to Resources page
+      </Link>
     </section>
   );
 }
