c4b6ef8e0878f5ac84e3f68d3776a2a455f89be6
Kendra and Kiana <kianacheatham@gmail.com>
2026-01-21 23:18:06 -0800

    updated footer code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-b p-4 bg-gray-100">
-      <h1 className="text-xl font-bold">USAC AAC Website</h1>
-     <h3>
-        USAC Academic Affairs Commission & Copyright. Or see the original website for reference.
-     </h3>
+      <h3>USAC Academic Affairs Commission & Copyright. Or see the original website for reference.</h3>
     </footer>
   );
 }
