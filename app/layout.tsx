8d88afce65538114bd047421de8f56b1ac0491de
Shogo-Toiyama <shogo.toiyama@gmail.com>
2026-01-22 17:50:50 -0800

    Created global navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Header />
+        <Nav/>
         <main className="min-h-screen p-6">{children}</main>
         <Footer />
       </body>
