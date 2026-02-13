47ad506312bf5279bdb5a19a1bb99d21b31dd32e
hillarynve <hillaryan@g.ucla.edu>
2026-02-12 16:33:04 -0800

    fixing main's page.tsx to show sarp page

--- app/sarp/page.tsx
+++ app/sarp/page.tsx
@@ -5,7 +5,7 @@ export default function SARP() {
     <section>
       <h1 className="text-2xl font-bold">AAC SARP</h1>
       <p className="mt-4 text-gray-600">
-        Placeholder page for resources content.
+        Placeholder page for SARP content.
       </p>
 
       <Link href="/" className="mt-4 inline-block text-blue-600 hover:underline">
