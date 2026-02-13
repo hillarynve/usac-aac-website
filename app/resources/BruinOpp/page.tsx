673b15c9435dbe9df6a9de46f9df0463dd361011
ElHombreZ <kaypro1932@gmail.com>
2026-02-12 17:13:58 -0800

    Basic stuff

--- app/resources/BruinOpp/page.tsx
+++ app/resources/BruinOpp/page.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 export default function BruinOpp() {
   return (
     <section>
-      <h1 className="text-2xl font-bold"> Bruin Opp Page</h1>
-      <p className="mt-4 text-gray-600">
-        Placeholder page for Bruin Opp content.
-      </p>
+      <h1 className="text-2xl font-bold"> BruinOpp</h1>
 
+      <image></image>
+      <p className="mt-4 text-gray-600">
+        Program Description
+      </p>
+      <br></br>
+      <button>
+        CTA
+      </button>
+      <br></br>
       <Link href="/" className="mt-4 inline-block text-blue-600 hover:underline">
         Back to Home </Link>
     </section>
