849238e8f53a0a18941353e7e0ac2420a89f8e84
hillarynve <hillaryan@g.ucla.edu>
2026-02-19 17:26:53 -0800

    initializing homepage branch

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section>
-      <h1 className="text-2xl font-bold">Welcome to USAC AAC</h1>
+      <h1 className="text-2xl font-bold">Welcome to USAC AAC Time to work on homepage</h1>
       <p className="mt-4 text-gray-600">
         Placeholder page for leadership content.
       </p>
