d29832c14ae0ced72e44f0e1e845c903850a461e
Kendra and Kiana <kianacheatham@gmail.com>
2026-02-23 09:37:14 -0800

    full SARP page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -15,7 +15,7 @@ export default function Nav() {
               </Link>
             </li>
             <li>
-              <Link href="/sarp">
+              <Link href="/SARP">
                 AACP SARP
               </Link>
             </li>
@@ -32,41 +32,4 @@ export default function Nav() {
           </ul>
     </nav>
   );
-}
-
-function NavButton({ label, href, dropdownItems }: NavButtonProps) {
-  if (!dropdownItems) {
-    return (
-      <li>
-        <Link href={href} className="inline-flex items-center justify-center h-12 min-w-40 px-8 bg-gray-300 hover:bg-gray-400">
-          {label}
-        </Link>
-      </li>
-    );
-  }
-
-  return (
-    <li className="relative group">
-      <Link href={href} className="inline-flex items-center justify-center h-12 min-w-32 px-8 bg-gray-300 hover:bg-gray-400">
-        {label}
-      </Link>
-      <div className="absolute top-full left-0 hidden group-hover:block bg-white shadow-lg min-w-[200px]">
-        {dropdownItems.map((item) => (
-          <Link 
-            key={item.href}
-            href={item.href} 
-            className="block px-4 py-2 hover:bg-gray-100"
-          >
-            {item.label}
-          </Link>
-        ))}
-      </div>
-    </li>
-  );
-}
-
-interface NavButtonProps {
-  label: string;
-  href: string;
-  dropdownItems?: { label: string; href: string }[];
 }
