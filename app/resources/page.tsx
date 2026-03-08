4c2aefdaed0698a74dfd209ee961408ea60076a9
ElHombreZ <kaypro1932@gmail.com>
2026-03-08 14:13:19 -0700

    Finished Resource Landing Page midfi, left the links below

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,10 +3,69 @@ import Link from "next/link";
 export default function Resources() {
   return (
     <section>
-      <h1 className="text-2xl font-bold">Hotlines & Getting Help</h1>
-      <p className="mt-4 text-gray-600">
-        Placeholder page for resources content.
-      </p>
+      <h1 className="text-[40px] font-medium px-16 text-left">Resources & Guides</h1>
+
+      <div className="flex flex-row gap-11 px-[60px] py-5 w-full justify-center">
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">Hotlines</p>
+          </div>
+        </div>
+
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">The UCLA Wellness Guide</p>
+          </div>
+        </div>
+
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">The UCLA Survival Guide</p>
+          </div>
+        </div>
+
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">Student Resource Database</p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="flex flex-row gap-11 px-[60px] py-5 w-full justify-center">
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">Disabled Student Union Guide</p>
+          </div>
+        </div>
+
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">UCLA Faculty Survey Report</p>
+          </div>
+        </div>
+
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">Student Resource Database</p>
+          </div>
+        </div>
+
+        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
+          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
+          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
+            <p className="text-white text-xl font-medium">Student Resource Database</p>
+          </div>
+        </div>
+      </div>
 
       <Link href="/resources/BruinOpp" className="mt-4 inline-block text-blue-600 hover:underline">
         Bruin Opp </Link><br></br>
