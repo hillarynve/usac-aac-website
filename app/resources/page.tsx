3bafd1c7da4bbbda0053962ce15d42f32513b387
ElHombreZ <kaypro1932@gmail.com>
2026-03-08 14:28:34 -0700

    Refactored the resouce cards

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ResourceCard from "./components/resourceCards";
 
 export default function Resources() {
   return (
@@ -6,65 +7,18 @@ export default function Resources() {
       <h1 className="text-[40px] font-medium px-16 text-left">Resources & Guides</h1>
 
       <div className="flex flex-row gap-11 px-[60px] py-5 w-full justify-center">
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">Hotlines</p>
-          </div>
-        </div>
-
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">The UCLA Wellness Guide</p>
-          </div>
-        </div>
-
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">The UCLA Survival Guide</p>
-          </div>
-        </div>
-
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">Student Resource Database</p>
-          </div>
-        </div>
+        <ResourceCard label="Hotlines" />
+        <ResourceCard label="The UCLA Wellness Guide" />
+        <ResourceCard label="The UCLA Survival Guide" />
+        <ResourceCard label="Student Resource Database" />
       </div>
 
 
-
       <div className="flex flex-row gap-11 px-[60px] py-5 w-full justify-center">
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">Disabled Student Union Guide</p>
-          </div>
-        </div>
-
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">UCLA Faculty Survey Report</p>
-          </div>
-        </div>
-
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">Student Resource Database</p>
-          </div>
-        </div>
-
-        <div className= "flex-1 flex flex-col rounded-[10px] overflow-hidden">
-          <div className="min-h-[189px] bg-[#B3B3B3]"></div>
-          <div className="bg-[#6E6E6E] px-[25px] py-[25px]">
-            <p className="text-white text-xl font-medium">Student Resource Database</p>
-          </div>
-        </div>
+        <ResourceCard label="Disabled Student Union Guide" />
+        <ResourceCard label="UCLA Faculty Survey Report" />
+        <ResourceCard label="Student Resource Database" />
+        <ResourceCard label="Student Resource Database" />
       </div>
 
       <Link href="/resources/BruinOpp" className="mt-4 inline-block text-blue-600 hover:underline">
