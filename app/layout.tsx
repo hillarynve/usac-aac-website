358ab97a068c24b42264f8f4110e514cacd5416b
hillarynve <hillaryan@g.ucla.edu>
2026-04-27 16:40:23 -0700

    bug fixing new header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import './globals.css';
+import { Inter } from "next/font/google";
+import "./globals.css";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={inter.className}> 
+    <html lang="en" className={inter.className}>
       <body>
         <Header />
-          <main className="min-h-screen p-6">{children}</main>
-        <Footer />
+        <main className="min-h-screen p-6">{children}</main>
       </body>
     </html>
   );
